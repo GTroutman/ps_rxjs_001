51232c7a2f9cc0a3cdda4a71c69c2b8c48391fd3
RIBAC\robinsom <mark.robinson@ribaenterprises.com>
2017-07-19 16:53:08 +0100

    Revisiting the retryWhen strategy module 04 lesson 04 - works

--- main.ts
+++ main.ts
@@ -1,70 +1,28 @@
 import { Observable } from "rxjs";
 import { load, loadWithFetch } from "./loader"
 
-// Will stop after error
-let source = Observable.merge(
-  Observable.of(1),
-  Observable.from([2,3,4]),
-  Observable.throw(new Error("Stop!")),
-  Observable.of(5)
-);
+let output = document.getElementById("output");
+let button = document.getElementById("button")
 
-source.subscribe(
-  value => console.log(`value: ${value}`),
-  error => console.log(`error: ${error}`),
+let click = Observable.fromEvent(button, "click");
+
+function renderMovies(movies) {
+    movies.forEach(m => {
+        let div = document.createElement("div");
+        div.innerText = m.title;
+        output.appendChild(div);
+    });
+}
+
+// Try to GET none existent file ...
+loadWithFetch("nowt.json")
+    .subscribe(renderMovies,
+        e => console.log(`error: ${e}`),
+        () => console.log("complete!"));
+
+click.flatMap(e => loadWithFetch("movies.json"))
+    .subscribe(
+        renderMovies,
+    e => console.log(`error: ${e}`),
     () => console.log("complete")
-);
-
-// Swallow error and continue on
-let source2 = Observable.onErrorResumeNext(
-    Observable.of(1),
-    Observable.from([2,3,4]),
-    Observable.throw(new Error("Stop!")),
-    Observable.of(5)
-);
-
-source2.subscribe(
-    value => console.log(`value: ${value}`),
-    error => console.log(`error: ${error}`),
-    () => console.log("complete")
-);
-
-// Catch error then return new Observable
-let source3 = Observable.merge(
-    Observable.of(1),
-    Observable.from([2,3,4]),
-    Observable.throw(new Error("Stop!")),
-    Observable.of(5)
-).catch(e => {
-    console.log(`caught: ${e}`);
-    return Observable.of(10);
-})
-
-source3.subscribe(
-    value => console.log(`value: ${value}`),
-    error => console.log(`error: ${error}`),
-    () => console.log("complete")
-);
-
-
-
-
-// let output = document.getElementById("output");
-// let button = document.getElementById("button")
-//
-// let click = Observable.fromEvent(button, "click");
-//
-// function renderMovies(movies) {
-//     movies.forEach(m => {
-//         let div = document.createElement("div");
-//         div.innerText = m.title;
-//         output.appendChild(div);
-//     });
-// }
-//
-// click.flatMap(e => loadWithFetch("movies.json"))
-//     .subscribe(
-//         renderMovies,
-//     e => console.log(`error: ${e}`),
-//     () => console.log("complete")
-//     );
+    );
