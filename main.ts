cae4f2b3405e40e702a65b03b523721cd361914d
RIBAC\robinsom <mark.robinson@ribaenterprises.com>
2017-07-19 16:23:52 +0100

    Dealing with errors and exceptions module 04 lesson 03 - works

--- main.ts
+++ main.ts
@@ -1,64 +1,70 @@
 import { Observable } from "rxjs";
+import { load, loadWithFetch } from "./loader"
 
-let output = document.getElementById("output");
-let button = document.getElementById("button")
+// Will stop after error
+let source = Observable.merge(
+  Observable.of(1),
+  Observable.from([2,3,4]),
+  Observable.throw(new Error("Stop!")),
+  Observable.of(5)
+);
 
-let click = Observable.fromEvent(button, "click");
-
-function load(url: string) {
-
-    return Observable.create(observer => {
-        let xhr = new XMLHttpRequest();
-
-        xhr.addEventListener("load", () => {
-
-            if (xhr.status === 200) {
-                let data = JSON.parse(xhr.responseText);
-                observer.next(data);
-                observer.complete();
-            } else {
-                observer.error(xhr.statusText);
-            }
-        });
-
-        xhr.open("GET", url);
-        xhr.send();
-    }).retryWhen(retryStrategy({ attempts: 3, delay: 1500 }));
-}
-
-function loadWithFetch(url: string){
-    // Observable.defer() - make observable lazy
-    return Observable.defer(() => {
-        return Observable.fromPromise(fetch(url).then(r => r.json()));
-    });
-}
-
-function retryStrategy({ attempts = 4, delay = 1000 }) {
-    return function(errors) {
-        return errors
-            .scan((acc, value) => {
-                console.log(acc, value);
-                return acc + 1;
-            }, 0)
-            .takeWhile(acc => acc < attempts)
-            .delay(delay);
-    }
-}
-
-function renderMovies(movies) {
-    movies.forEach(m => {
-        let div = document.createElement("div");
-        div.innerText = m.title;
-        output.appendChild(div);
-    });
-}
-
-loadWithFetch("movies.json");   // Calls function but nothing happens until the observable is subscribed to (due to
-                                    // Observable.defer().
-
-click.flatMap(e => loadWithFetch("movies.json"))
-    .subscribe(
-        renderMovies,
-    e => console.log(`error: ${e}`),
+source.subscribe(
+  value => console.log(`value: ${value}`),
+  error => console.log(`error: ${error}`),
     () => console.log("complete")
-    );
+);
+
+// Swallow error and continue on
+let source2 = Observable.onErrorResumeNext(
+    Observable.of(1),
+    Observable.from([2,3,4]),
+    Observable.throw(new Error("Stop!")),
+    Observable.of(5)
+);
+
+source2.subscribe(
+    value => console.log(`value: ${value}`),
+    error => console.log(`error: ${error}`),
+    () => console.log("complete")
+);
+
+// Catch error then return new Observable
+let source3 = Observable.merge(
+    Observable.of(1),
+    Observable.from([2,3,4]),
+    Observable.throw(new Error("Stop!")),
+    Observable.of(5)
+).catch(e => {
+    console.log(`caught: ${e}`);
+    return Observable.of(10);
+})
+
+source3.subscribe(
+    value => console.log(`value: ${value}`),
+    error => console.log(`error: ${error}`),
+    () => console.log("complete")
+);
+
+
+
+
+// let output = document.getElementById("output");
+// let button = document.getElementById("button")
+//
+// let click = Observable.fromEvent(button, "click");
+//
+// function renderMovies(movies) {
+//     movies.forEach(m => {
+//         let div = document.createElement("div");
+//         div.innerText = m.title;
+//         output.appendChild(div);
+//     });
+// }
+//
+// click.flatMap(e => loadWithFetch("movies.json"))
+//     .subscribe(
+//         renderMovies,
+//     e => console.log(`error: ${e}`),
+//     () => console.log("complete")
+//     );
